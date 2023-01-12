65c1a86fd8a8d88ac0ef86f189cd1ceed45e4698
scarf <greenscarf005@gmail.com>
2023-01-12 20:40:36 +0900

    fix: 빈 id는 선택하지 않음

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,13 @@
 import { TodoPreview } from 'components'
-import { SimpleGrid, Skeleton, Stack, Textarea, Title } from '@mantine/core'
+import {
+  Paper,
+  SimpleGrid,
+  Skeleton,
+  Stack,
+  Text,
+  Textarea,
+  Title,
+} from '@mantine/core'
 import { getTodoById, getTodos } from 'api'
 import { useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
@@ -10,14 +18,15 @@ const todoQuery = (id: string) => ({
   queryFn: () => getTodoById(id),
 })
 
-export const Editor = () => {
-  
-}
+// export const TodoTextArea = ({ initial }: { initial: string }) => {
+//   const [text, setText] = useState(initial)
 
-export const Content = () => {
-  const location = useLocation()
-  const id = location.pathname.split('/').at(-1) as string
+//   return (
+//     <Textarea value={text} onChange={(e) => setText(e.currentTarget.value)} />
+//   )
+// }
 
+export const TodoContent = ({ id }: { id: string }) => {
   const { data: todo, isSuccess } = useQuery(todoQuery(id))
 
   if (!isSuccess) {
@@ -25,11 +34,22 @@ export const Content = () => {
   }
 
   return (
-    <Stack>
+    <>
       <Title>{todo.title}</Title>
-      <Textarea />
-    </Stack>
+      <Text>{todo.content}</Text>
+      {/* <TodoTextArea initial={todo.content} /> */}
+    </>
   )
+}
+
+export const Content = () => {
+  const location = useLocation()
+  const id = location.pathname.split('/').at(-1) as string
+
+  const detail =
+    id !== '' ? <TodoContent id={id} /> : <Title>TODO를 선택해주세요</Title>
+
+  return <Paper>{detail}</Paper>
 }
 
 export const Home = () => {
