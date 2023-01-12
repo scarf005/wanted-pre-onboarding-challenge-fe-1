c664e1345cac0a912bf47ddf57f3da29cdbf5c82
scarf <greenscarf005@gmail.com>
2023-01-12 20:40:04 +0900

    fix: redirect -> navigate

--- src/components/EnforceAuth.tsx
+++ src/components/EnforceAuth.tsx
@@ -1,13 +1,16 @@
-import { redirect } from 'react-router-dom'
+import { redirect, useNavigate } from 'react-router-dom'
 import { useAtomValue } from 'jotai'
 import { isLoggedInAtom } from 'atom'
 import { Button } from '@mantine/core'
+import { paths } from 'routes/paths'
 
 export const EnforceAuth = ({
   children,
-}: { children: JSX.Element }): JSX.Element =>
-  useAtomValue(isLoggedInAtom) ? (
+}: { children: JSX.Element }): JSX.Element => {
+  const navigate = useNavigate()
+  return useAtomValue(isLoggedInAtom) ? (
     children
   ) : (
-    <Button onClick={() => redirect('/auth')}>로그인해 주세요</Button>
+    <Button onClick={() => navigate(paths.login)}>로그인해 주세요</Button>
   )
+}
