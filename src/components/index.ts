38df4d705145638ca4b762a2bd257ee8d8966fbd
scarf <greenscarf005@gmail.com>
2023-01-16 15:05:34 +0900

    refactor: HOC 컴포넌트 정리

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export * from './AuthButton'
-export * from './RequireAuth'
-export * from './RequireNoAuth'
+export * from './hoc'
 export * from './TodoItem'
 export * from './TodoPreview'
 export * from './TodoSkeleton'
