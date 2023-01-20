9b9cf00e4d04924c24377d7c2f32f9ae928a3f38
scarf <greenscarf005@gmail.com>
2023-01-20 09:59:15 +0900

    refactor: `TodoEdit` -> `CreateTodo` 이름 변경
    생성을 위한 컴포넌트이므로 `CreateTodo`로 이름 변경

--- src/components/CreateTodo/components/TodoEditModal.stories.tsx
+++ src/components/CreateTodo/components/TodoEditModal.stories.tsx
@@ -13,13 +13,13 @@ export const TodoEditModal = () => {
         title='새 TODO 추가하기'
       >
         <TodoForm
-          button={<Button>수정하기</Button>}
-          onSubmit={v => alert(`sent ${JSON.stringify(v)}`)}
+          button={<Button>추가하기</Button>}
+          onSubmit={(v) => alert(`sent ${JSON.stringify(v)}`)}
         />
       </Modal>
 
       <Group position='right'>
-        <Button onClick={() => setOpened(true)}>Open Modal</Button>
+        <Button onClick={() => setOpened(true)}>새 TODO 추가하기</Button>
       </Group>
     </>
   )
